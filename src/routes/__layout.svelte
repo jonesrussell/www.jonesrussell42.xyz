46cef984aaeba8aac8f58c762292d4554c4fccef
Russell Jones <jonesrussell42@gmail.com>
2022-02-06 20:21:01 -0500

    Add Tailwind

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,45 +1,50 @@
-<script lang="ts">
-	import Header from '$lib/header/Header.svelte';
-	import '../app.css';
+<script context="module">
+	export const preload = () => {};
 </script>
 
-<Header />
+<script lang="ts">
+	import '../app.css';
+	import Nav from '../components/Nav.svelte';
+	import Footer from '../components/Footer.svelte';
+	import { GoogleAnalytics } from '@beyonk/svelte-google-analytics';
+	import { getStores, navigating, page, session, updated } from '$app/stores';
+	import LogRocket from 'logrocket';
+	import Bugsnag from '@bugsnag/js';
 
-<main>
-	<slot />
+	export let segment: string = 'home';
+	export let year: number = new Date().getFullYear();
+	let ga_measurment_id = 'UA-114644797-1';
+
+	LogRocket.init('herbig-haro/portfolio');
+
+	if (!Bugsnag._client) {
+		Bugsnag.start({ apiKey: '615a4defdc1b405abb1743b6cb33843d', logger: null });
+	}
+
+	Bugsnag.beforeNotify = (data: { metaData: { sessionURL: string | null } }) => {
+		data.metaData.sessionURL = LogRocket.sessionURL;
+		return data;
+	};
+</script>
+
+<Nav {segment} />
+
+<section class="relative block hero-home" />
+
+<main class="relative sasquatch">
+	<div class="container mx-auto px-4 max-w-screen-lg m-10 py-10">
+		<div
+			class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-xl rounded-lg -mt-64"
+		>
+			<div class="px-6">
+				<slot />
+			</div>
+		</div>
+	</div>
 </main>
 
-<footer>
-	<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
-</footer>
+<Footer {year} />
 
-<style>
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 1024px;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
-
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 40px;
-	}
-
-	footer a {
-		font-weight: bold;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 40px 0;
-		}
-	}
-</style>
+<svelte:head>
+	<GoogleAnalytics properties={[ga_measurment_id]} />
+</svelte:head>
