2dd1847fd08e89b3ac0b401286b272d286ce285c
Russell Jones <jonesrussell42@gmail.com>
2022-02-06 21:30:41 -0500

    WIP

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,22 @@
 module.exports = {
-  apps: [
-    {
-      name: 'portfolio-svelte',
-      script: './build/index.js',
-      watch: false,
-      force: true,
-      env: {
-        PORT: 4500,
-        NODE_ENV: 'production',
-      },
-    },
-  ],
-};
+  apps : [{
+    script: './build/index.js',
+    watch: './build'
+  }, {
+    script: './service-worker/',
+    watch: ['./service-worker']
+  }],
 
+  deploy : {
+    production : {
+      user : 'SSH_USERNAME',
+      host : 'SSH_HOSTMACHINE',
+      ref  : 'origin/master',
+      repo : 'GIT_REPOSITORY',
+      path : 'DESTINATION_PATH',
+      'pre-deploy-local': '',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': ''
+    }
+  }
+};
