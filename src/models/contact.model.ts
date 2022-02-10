6605e9232245fba2707a7abee0bc675b9d87ef23
Russell Jones <jonesrussell42@gmail.com>
2022-02-10 10:09:13 +0000

    Fixed contact form and mongodb

--- src/models/contact.model.ts
+++ src/models/contact.model.ts
@@ -1,20 +1,20 @@
-import mongoose, { Schema, Document } from "mongoose";
+import mongoose, { Schema, Document } from 'mongoose';
 
 export interface IContact extends Document {
-  email: string;
-  message: string;
+	email: string;
+	message: string;
 }
 
 const ContactSchema: Schema = new Schema({
-  email: {
-    type: String,
-    required: true,
-  },
-  message: {
-    type: String,
-    required: true,
-  },
+	email: {
+		type: String,
+		required: true
+	},
+	message: {
+		type: String,
+		required: true
+	}
 });
 
 // return IContact interface
-export default mongoose.model<IContact>("Contact", ContactSchema);
+export default mongoose.model<IContact>('Contact', ContactSchema);
