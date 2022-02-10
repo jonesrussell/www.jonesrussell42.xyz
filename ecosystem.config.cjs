a04ae49167c1924dcec87e9283e294c0a71d342b
Russell Jones <jonesrussell42@gmail.com>
2022-02-10 08:28:29 +0000

    Set mongodb uri

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,11 +1,14 @@
 module.exports = {
-  apps : [{
-    name: 'portfolio',
-    script: './build/index.js',
-    watch: './build',
-    env: {
-       'PORT': 3100,
-       'NODE_ENV': 'production'
-    }
-  }]
-}
+	apps: [
+		{
+			name: 'portfolio',
+			script: './build/index.js',
+			watch: './build',
+			env: {
+				PORT: 3100,
+				NODE_ENV: 'production',
+				MONGODB_URI: 'mongodb://mongodb.jonesrussell42.xyz:27017/portfolio-app'
+			}
+		}
+	]
+};
