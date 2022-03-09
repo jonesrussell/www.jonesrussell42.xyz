e46544d244dedca2622664211d91aea3692a75e1
Russell Jones <jonesrussell42@gmail.com>
2022-03-09 04:32:09 +0000

    WIP

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,11 @@
 module.exports = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		extend: {
+			gridTemplateRows: {
+				'10': 'repeat(10, minmax(0, 1fr))'
+			}
+		}
 	},
 	plugins: []
 };
