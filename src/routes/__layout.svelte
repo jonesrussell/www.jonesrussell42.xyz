a02d6a521dcdbcf3f3da542b54ad17bb623764f2
Russell Jones <jonesrussell42@gmail.com>
2022-03-09 04:29:31 +0000

    Remove max width of conent area

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -31,11 +31,11 @@
 <section class="relative block hero-home" />
 
 <main class="relative sasquatch">
-	<div class="container mx-auto px-4 max-w-screen-lg m-10 py-10">
+	<div class="container mx-auto px-4 m-10 py-10">
 		<div
 			class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-xl rounded-lg -mt-64"
 		>
-			<div class="px-6">
+			<div class="px-6 py-6">
 				<slot />
 			</div>
 		</div>
