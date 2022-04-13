2834902a55b036fee3356c8d905d56a26664506d
Russell Jones <jonesrussell42@gmail.com>
2022-04-13 17:31:41 +0000

    Restore max width on content area

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -29,7 +29,7 @@
 <section class="relative block hero-home" />
 
 <main class="relative sasquatch">
-	<div class="container mx-auto px-4 m-10 py-10">
+	<div class="container mx-auto px-4 max-w-screen-lg m-10 py-10">
 		<div
 			class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-xl rounded-lg -mt-64"
 		>
